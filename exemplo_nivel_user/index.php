6e28fc914a4a07afc9da16ce91506cffc17f21df
Gabriel Darezzo <gabrieldarezzo@yahoo.com.br>
2017-04-26 20:14:06 -0300

    melhoria

--- exemplo_nivel_user/index.php
+++ exemplo_nivel_user/index.php
@@ -3,8 +3,6 @@
 /*
 create database imasters;
 use imasters;
-
-
 CREATE TABLE usuario(
 	 id  				int(8) primary key auto_increment
 	,nome 				varchar(50) not null
@@ -12,27 +10,10 @@ CREATE TABLE usuario(
 );
 
 
-CREATE TABLE usuario(
-	 id  				int(8) primary key auto_increment
-	,nome_completo 		varchar(50) not null
-	,email 				varchar(250) not null
-	,senha 				varchar(100) not null
-	,token				CHAR(32)
-	,criado_em			DATETIME
-);
-
-nome_completo:sadsadsadsadsadsa
-email:gabrieldarezzo@yahoo.com.br
-senha:dasdasdsadasdas
-
-
-
 INSERT INTO usuario(nome, niveis_acesso_id) values 
 	('Cláudio', 1) 
 	,('João', 2) 
 ;
-
-
 select * from usuario;
 +----+---------+------------------+
 | id | nome    | niveis_acesso_id |
@@ -41,33 +22,25 @@ select * from usuario;
 |  2 | João    |                2 |
 +----+---------+------------------+
 */
-
 	
-
 $db = new PDO('mysql:host=localhost;dbname=imasters', 'root', '');
-$db->exec("set names utf8"); //Garante UTF em versão < 5.3
-
+$db->exec("set names utf8");
 
 $stmt = $db->prepare("select id, niveis_acesso_id from usuario where id = :id");
 //No seu ....
-//"select * from usuario where nome = ? AND SENHA = ?"
 
 
 $id = 1; // Busca Claudio
 $stmt->bindParam(':id', $id, PDO::PARAM_INT);   
 $stmt->execute();
-
 $usuario = $stmt->fetch(PDO::FETCH_OBJ);
-
 if(!$usuario){
 	die('Usuario não encontrado.');
 }
-
-if(!$usuario){
-	die('Usuario não encontrado.');
-}
-
 
 if($usuario->niveis_acesso_id < 2){
 	die("Apenas user 'ADM'");	
 }
+
+//A partir daqui sabemos que o usuario 1 (Claudio) tem permissão de acessar
+echo 'APENAS USER NIVEL 2 CONSEGUE VER ISSO';
